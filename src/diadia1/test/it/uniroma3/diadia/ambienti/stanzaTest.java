3389ffd8bfd8f82d6b8f1d757fb6d7d343426f38
steffede <mozzillostefano7@gmail.com>
2026-04-02 17:34:08 +0200

    Add files via upload

--- src/diadia1/test/it/uniroma3/diadia/ambienti/stanzaTest.java
+++ src/diadia1/test/it/uniroma3/diadia/ambienti/stanzaTest.java
@@ -12,88 +12,132 @@ import org.junit.jupiter.api.BeforeEach;
 
 class stanzaTest {
 private Stanza stanza;
-private Stanza cucina;
-private Attrezzo spada;
-private Attrezzo elmo;
+private Stanza stanzaAdiacente;
+private Stanza stanzaConUnMartello;
+private Attrezzo martello;
+
+
 
 
 	@BeforeEach
 	void setUp() {
 	stanza=new Stanza("stanza");
-	cucina=new Stanza("cucina");
-	spada= new Attrezzo("spada", 5);
-	elmo= new Attrezzo("elmo",2);
+	stanzaConUnMartello=new Stanza("stanza con un martello");
+	martello= new Attrezzo("martello", 5);
+	stanzaConUnMartello.addAttrezzo(martello);
+	stanzaAdiacente= new Stanza("stanza adiacente");
+	
 	
 	
 	}
 	
-
+//hasAttrezzo e addAttrezzo
     @Test
     void testLeStanzeVuoteNonHannoAttrezzi() {
-	
 	assertFalse(new Stanza("vuota").hasAttrezzo("martello"));
-    }
-    @Test
-    void testStanzaConUnAttrezzo_trovato() {
-    	final Stanza piena=new Stanza("piena");
-        assertFalse(piena.hasAttrezzo("martello"));
-        piena.addAttrezzo(new Attrezzo("martello",10));
-        assertTrue(piena.hasAttrezzo("martello"));
     }
     
     @Test
-    void testStanzaConUnAttrezzo_introvabile() {
-    	final Stanza piena=new Stanza("piena");
-        piena.addAttrezzo(new Attrezzo("martello",10));
-        assertFalse(piena.hasAttrezzo("introvabile"));
-    }
-    @Test
     void testMassimo10Attrezzi() {
-    	final Stanza piena=new Stanza("piena");	
+    	final Stanza piena=new Stanza("piena");
     	for(int i=0; i<10;i++) {
     		assertTrue(piena.addAttrezzo(new Attrezzo("Attrezzo_" +i, i)));
     	}
     	assertFalse(piena.addAttrezzo(new Attrezzo("escluso",1)));
     }
-	
+    
+    @Test
+    void attrezzoAggiuntoCorrettamente() {
+		assertTrue(stanza.addAttrezzo(martello));
+		assertTrue(stanza.hasAttrezzo("martello"));
+    }
+    
+    @Test
+    void testStanzaConUnAttrezzo_trovato() {
+		assertTrue(stanzaConUnMartello.hasAttrezzo("martello"));
+    }
+    
+    @Test
+    void testStanzaConUnAttrezzo_introvabile() {
+        assertFalse(stanzaConUnMartello.hasAttrezzo("introvabile"));
+    }
+    
+   
+ 
+//removeAttrezzo	
     @Test
     void testRimuoviAttrezzoDaStanzaConUnAttrezzo_trovato() {
-    	final Stanza piena=new Stanza("piena");
-    	piena.addAttrezzo(elmo);
-    	assertTrue(piena.removeAttrezzo(elmo));
+    	assertTrue(stanzaConUnMartello.removeAttrezzo(martello));
+    	assertFalse(stanzaConUnMartello.hasAttrezzo("martello"));
     }
     
     @Test
     void testRimuoviAttrezzo_introvabile() {
-    	final Stanza vuota=new Stanza("vuota");
-    	assertFalse(vuota.removeAttrezzo(elmo));
+    	assertFalse(stanzaConUnMartello.removeAttrezzo(new Attrezzo("spada",4)));
+    }
+    
+    @Test
+    void testRimuoviAttrezzoDaStanzaVuota() {
+    	assertFalse(new Stanza("vuota").removeAttrezzo(martello));
     }
     
     
-    
+//getAttrezzo
+   @Test
+   void testGetAttrezzo_trovato() {
+	   assertEquals(martello,stanzaConUnMartello.getAttrezzo("martello"));
+   }
+ 
+   @Test
+   void testGetAttrezzo_introvabile() {
+	   assertNull(stanzaConUnMartello.getAttrezzo("spada"));
+   }
+   
+//impostaStanzaAdiacente   e getStanzaAdiacente 
     @Test
-	public void testImpostaStanzaAdiacenteNordCorrettamente() {
-		stanza.impostaStanzaAdiacente("nord", cucina);
-		assertEquals(cucina,stanza.getStanzaAdiacente("nord"), "inserita stanza nord");
+	void testImpostaStanzaAdiacenteNordCorrettamente() {
+		stanza.impostaStanzaAdiacente("nord", stanzaAdiacente);
+		assertEquals(stanzaAdiacente,stanza.getStanzaAdiacente("nord"), "inserita stanza nord");
 	}
 	@Test
-	public void testImpostaStanzaAdiacenteSudCorrettamente() {
-		stanza.impostaStanzaAdiacente("sud",cucina);
-		assertEquals(cucina,stanza.getStanzaAdiacente("sud"));
-	}
-	
-	@Test
-	public void testImpostaStanzaAdiacenteEstCorrettamente() {
-		stanza.impostaStanzaAdiacente("est",cucina);
-		assertEquals(cucina,stanza.getStanzaAdiacente("est"));
+	void testImpostaStanzaAdiacenteSudCorrettamente() {
+		stanza.impostaStanzaAdiacente("sud",stanzaAdiacente);
+		assertEquals(stanzaAdiacente,stanza.getStanzaAdiacente("sud"));
 	}
 	
 	@Test
-	public void testImpostaStanzaAdiacenteOvestCorrettamente() {
-		stanza.impostaStanzaAdiacente("ovest",cucina);
-		assertEquals(cucina,stanza.getStanzaAdiacente("ovest"));
+	void testImpostaStanzaAdiacenteEstCorrettamente() {
+		stanza.impostaStanzaAdiacente("est",stanzaAdiacente);
+		assertEquals(stanzaAdiacente,stanza.getStanzaAdiacente("est"));
 	}
 	
+	@Test
+	void testImpostaStanzaAdiacenteOvestCorrettamente() {
+		stanza.impostaStanzaAdiacente("ovest",stanzaAdiacente);
+		assertEquals(stanzaAdiacente,stanza.getStanzaAdiacente("ovest"));
+	}
 	
+	@Test
+	void impostaMassimo4StanzeAdiacenti() {
+	    stanza.impostaStanzaAdiacente("nord", stanzaAdiacente);
+	    stanza.impostaStanzaAdiacente("sud", stanzaAdiacente);
+	    stanza.impostaStanzaAdiacente("est", stanzaAdiacente);
+	    stanza.impostaStanzaAdiacente("ovest", stanzaAdiacente);
+	    stanza.impostaStanzaAdiacente("nord-est", stanzaAdiacente); 
+	    assertNull(stanza.getStanzaAdiacente("nord-est")); 
+	}
+	
+	@Test
+	void aggiornaDirezioneGiàEsistente() {
+	    Stanza nuovaStanza = new Stanza("nuova");
+	    stanza.impostaStanzaAdiacente("nord", stanzaAdiacente);
+	    stanza.impostaStanzaAdiacente("nord", nuovaStanza);  // sovrascrive
+	    assertEquals(nuovaStanza, stanza.getStanzaAdiacente("nord"));
+	}
+	
+	@Test
+	void direzioneInesistente() {
+	    assertNull(stanza.getStanzaAdiacente("nord")); //stanza non ha stanze adiacenti
+	}
 
 }
