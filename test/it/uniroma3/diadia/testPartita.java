d191f0100b04f7db559e5151ae3ed46ccb8f8c4d
ValeCiap <ciaprini.valerio.14@gmail.com>
2026-04-02 18:43:11 +0200

    Update testPartita.java

--- test/it/uniroma3/diadia/testPartita.java
+++ test/it/uniroma3/diadia/testPartita.java
@@ -14,60 +14,30 @@ class testPartita {
 		this.partita = new Partita();
 	}
 
-	/**
-	 * Test che verifica se vinco la partita nella stanza finale
-	 * @return true
-	 */
+	
+//vinta()
 	@Test
 	void testPartitaVintaInStanzaFinale() {
 		this.partita.setStanzaCorrente(this.partita.getLabirinto().getStanzaFinale());
 		assertTrue(this.partita.vinta());
+		assertFalse(this.partita.vinta()==false);
 	}
 	
-	/**
-	 * Test che verifica se vinco la partita in un'altra stanza
-	 * @return false
-	 */
 	@Test
 	void testPartitaVintaNonInStanzaFinale() {
 		this.partita.setStanzaCorrente(this.partita.getLabirinto().getStanzaCorrente());
 		assertFalse(this.partita.vinta());
 	}
 	
-	/**
-	 * Test che verifica se non vinco la partita nella stanza finale
-	 * @return false
-	 */
-	@Test
-	void testPartitaNonVintaInStanzaFinale() {
-		this.partita.setStanzaCorrente(this.partita.getLabirinto().getStanzaFinale());
-		assertFalse(this.partita.vinta()==false);
-	}
 	
-	/**
-	 * Test che verifica se la partita finisce con 0 cfu rimasti
-	 * @return true
-	 */
+//isFinita()
 	@Test
 	void testPartitaFinitaCFU_0() {
 		this.partita.getGiocatore().setCfu(0);
 		assertTrue(this.partita.isFinita());
-	}
-	
-	/**
-	 * Test che verifica se la non partita finisce con 0 cfu rimasti
-	 * @return true
-	 */
-	@Test
-	void testPartitaNonFinitaCFU_0() {
-		this.partita.getGiocatore().setCfu(0);
 		assertFalse(this.partita.isFinita()==false);
 	}
-	
-	/**
-	 * Test che verifica se la partita finisce quando la vinco
-	 * @return true
-	 */
+
 	@Test
 	void testPartitaFinitaVinta() {
 		this.partita.setStanzaCorrente(this.partita.getLabirinto().getStanzaFinale());
