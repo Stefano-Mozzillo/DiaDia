745c6bf8432e339c6a95560d28825f1e5a686179
ValeCiap <ciaprini.valerio.14@gmail.com>
2026-05-04 18:52:42 +0200

    Update TestIOSimulator.java

--- diadia/src/it/uniroma3/diadia/TestIOSimulator.java
+++ diadia/src/it/uniroma3/diadia/TestIOSimulator.java
@@ -48,6 +48,45 @@ class TestIOSimulator {
 		gioco.gioca();
 		assertTrue(partita.getGiocatore().getBorsa().isEmpty());
 	}
-
+	
+	@Test
+	void testPartitaBase_PersaFineCFU() {
+		String[] comandi = new String[40];
+		for(int i=0; i<20; i++) {
+			comandi[i*2]="vai sud";
+			comandi[i*2+1]= "vai nord";
+		}
+		IOSimulator io = new IOSimulator(comandi);
+		DiaDia gioco=new DiaDia(io);
+		partita=gioco.getPartita();
+		gioco.gioca();
+		assertTrue(partita.isFinita());
+		assertFalse(partita.vinta());
+	}
+	
+	@Test
+	void testPartitaBase_PrendoOsso_CambioStanza_PosoOsso_TornoInAtrio() {
+		String[] comandi={"prendi osso","vai est","posa osso", "vai ovest", "vai nord"};
+		IOSimulator io= new IOSimulator(comandi);
+		DiaDia gioco=new DiaDia(io);
+		partita=gioco.getPartita();
+		gioco.gioca();
+		assertTrue(partita.getGiocatore().getBorsa().isEmpty());
+		assertTrue(partita.isFinita());
+		assertTrue(partita.vinta());
+	}
+	
+	@Test
+	void testPartitaBase_VadoSud_PrendoLanterna_TornoInAtrio_PrendoOsso_VadoNord() {
+		String[] comandi={"vai sud", "prendi lanterna", "vai nord", "prendi osso", "vai nord"};
+		IOSimulator io= new IOSimulator(comandi);
+		DiaDia gioco=new DiaDia(io);
+		partita=gioco.getPartita();
+		gioco.gioca();
+		assertTrue(partita.getGiocatore().getBorsa().hasAttrezzo("osso"));
+		assertTrue(partita.getGiocatore().getBorsa().hasAttrezzo("lanterna"));
+		assertTrue(partita.isFinita());
+		assertTrue(partita.vinta());
+	}
 
 }
