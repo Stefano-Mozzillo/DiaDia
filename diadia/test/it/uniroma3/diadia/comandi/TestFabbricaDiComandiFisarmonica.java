1424e2071485afbdfc7a578ec07cf47810430051
ValeCiap <ciaprini.valerio.14@gmail.com>
2026-05-04 18:51:52 +0200

    Update TestFabbricaDiComandiFisarmonica.java

--- diadia/test/it/uniroma3/diadia/comandi/TestFabbricaDiComandiFisarmonica.java
+++ diadia/test/it/uniroma3/diadia/comandi/TestFabbricaDiComandiFisarmonica.java
@@ -27,4 +27,34 @@ class TestFabbricaDiComandiFisarmonica {
 		assertEquals(factory.getNome(), "aiuto");
 		assertNull(factory.getParametro());
 	}
+	@Test
+	void testRiconoscimentoComandoPosa() {
+		factory.costruisciComando("posa attrezzo");
+		assertEquals(factory.getNome(), "posa");
+		assertEquals(factory.getParametro(), "attrezzo");
+	}
+	@Test
+	void testRiconoscimentoComandoFine() {
+		factory.costruisciComando("fine");
+		assertEquals(factory.getNome(), "fine");
+		assertNull(factory.getParametro());
+	}
+	@Test
+	void testRiconoscimentoComandoGuarda() {
+		factory.costruisciComando("guarda");
+		assertEquals(factory.getNome(), "guarda");
+		assertNull(factory.getParametro());
+	}
+	@Test
+	void testRiconoscimentoComandoNonValido() {
+		factory.costruisciComando("...");
+		assertEquals(factory.getNome(), "...");
+		assertNull(factory.getParametro());
+	}
+	@Test
+	void testRiconoscimentoComandoVuoto() {
+		factory.costruisciComando("");
+		assertNull(factory.getNome());
+		assertNull(factory.getParametro());
+	}
 }
