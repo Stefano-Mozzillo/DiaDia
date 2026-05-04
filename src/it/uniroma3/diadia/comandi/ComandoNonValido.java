1c0e3fcdae7647f9fbcf9139fea533ea2922c788
ValeCiap <ciaprini.valerio.14@gmail.com>
2026-05-04 17:16:58 +0200

    Add files via upload

---
+++ src/it/uniroma3/diadia/comandi/ComandoNonValido.java
@@ -0,0 +1,19 @@
+package it.uniroma3.diadia.comandi;
+
+import it.uniroma3.diadia.IO;
+import it.uniroma3.diadia.Partita;
+
+public class ComandoNonValido implements Comando {
+
+	@Override
+	public void esegui(Partita partita, IO console) {
+		console.mostraMessaggio("comando non valido, riprova");
+		
+	}
+
+	@Override
+	public void setParametro(String parametro) {
+		
+	}
+
+}
